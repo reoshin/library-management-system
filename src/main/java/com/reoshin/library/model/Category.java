e9e21bb1faf0456c0da5ca70ede07b0367497da6
reoshin <s_reo6@outlook.com>
2026-07-31 21:26:57 +0900

    Created new Category enum

---
+++ src/main/java/com/reoshin/library/model/Category.java
@@ -0,0 +1,13 @@
+package com.reoshin.library.model;
+
+public enum Category {
+    FICTION,
+    NON_FICTION,
+    SCIENCE,
+    HISTORY,
+    FANTASY,
+    BIOGRAPHY,
+    MYSTERY,
+    ROMANCE,
+    TECHNOLOGY
+}
