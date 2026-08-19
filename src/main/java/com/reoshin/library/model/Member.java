b749362f0e059f710989e899e79df53e2d3e1f64
reoshin <s_reo6@outlook.com>
2026-08-19 21:07:10 +0900

    Implemented Member Class

--- src/main/java/com/reoshin/library/model/Member.java
+++ src/main/java/com/reoshin/library/model/Member.java
@@ -1,4 +1,37 @@
 package com.reoshin.library.model;
 
-public class Member {
+import java.util.ArrayList;
+
+public class Member implements LoanObserver {
+    private String userID;
+    private String userName;
+    private ArrayList<LibraryItem> loanList;
+    private Boolean activated;
+
+    // EFFECTS: constructs user with given name and userNumber.
+    public Member(int userNumber, String name) {
+        this.userID = String.format("%05d", userNumber);
+        this.loanList = new ArrayList<>();
+        this.userName = name;
+        this.activated = true;
+    }
+
+    public String getUserID() {
+        return this.userID;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public Boolean isActivated() {
+        return this.activated;
+    }
+
+    @Override
+    public void onItemLoaned(LibraryItem item, Member member) {
+        loanList.add(item);
+    }
+
+    
 }
