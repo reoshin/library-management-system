bd473b691c429fda2649e7a08bb95350f7e6214e
reoshin <s_reo6@outlook.com>
2026-08-20 19:15:11 +0900

    Console UI development #2 (Register UI & fixed typo)

--- src/main/java/com/reoshin/library/ui/LibraryLegacyUI.java
+++ src/main/java/com/reoshin/library/ui/LibraryLegacyUI.java
@@ -8,6 +8,7 @@ import com.reoshin.library.model.Member;
 public class LibraryLegacyUI {
     private Library lib = new Library();
     private Scanner scanner;
+    private Member myUser = null;
     
     public LibraryLegacyUI() {
         lib.newMember("Admin"); // 00000
@@ -27,48 +28,76 @@ public class LibraryLegacyUI {
     }
 
     public void runApp() {
+        Boolean keepGoing = true;
         String input = null;
         scanner = new Scanner(System.in);
 
-
-        displayMemu();
-        input = scanner.next();
-        input = input.toLowerCase();
-        processCommand(input);
+        while (keepGoing) {
+            displayMenu();
+            input = scanner.next();
+            input = input.toLowerCase();
+            processCommand(input);
+        }
     }
 
     public void login() {
-        
         System.out.println("Enter your ID");
         String id = scanner.next();
-        Member myUser = null;
         try {
             myUser = lib.getMember(id);
         } catch (Exception e) {
-            System.out.println("Login Failed!");
-            System.out.println("Moving back to the main screen...");
+            System.out.println("Login failed.");
+            System.out.println("Please Check your ID again!");
             login();
             return;
         }
         if (myUser.getUserID().equals("00000")) {
-            openAdminUI();
-        } else {
-            loginWithUser(myUser);
+            myUser = null;
+            adminLogin();
         }
     }
 
-    public void loginWithUser(Member myUser) {
-        System.out.println("Login Success.");
-        System.out.println("Hello, " + myUser.getUserName());
-    }
 
+    // Developer's setting.
     public void openAdminUI() {
-        System.out.println("Maintainerance Mode");
-        System.out.println("");
+        System.out.println("'a' - add new item to library");
+        System.out.println("'x' - add new item to library");
     }
 
-    public void displayMemu() {
-        
+    public void openGuestUI() {
+        System.out.println("Hello, Guest");
+        System.out.println("Enter 'l' to Login");
+        System.out.println("Not a member? Type 'r' to get started!");
+    }
+
+    public void openMemberUI() {
+        System.out.println("Hello, " + myUser.getUserName() + "(ID: " + myUser.getUserID() + ")");
+    }
+
+    public void adminLogin() {
+        System.out.println("Maintenance Mode");
+        System.out.println("Type 'q' if you didn't mean to login as admin.");
+        String passcode = "";
+        while (! passcode.equals("1234")) {
+            System.out.println("Passcode: ");
+            passcode = scanner.next();
+            if (passcode.equals("q")) {
+                System.out.println("Login failed.");
+                return;
+            }
+            System.out.println("Try again.");
+        }
+        myUser = lib.getMember("00000");
+    }
+
+    public void displayMenu() {
+        if (myUser == null) {
+            openGuestUI();
+        } else if (myUser.getUserID().equals("00000")) {
+            openAdminUI();
+        } else {
+            openMemberUI();
+        }
     }
 
     public void processCommand(String input) {
@@ -80,6 +109,12 @@ public class LibraryLegacyUI {
     }
 
     public void register() {
-
+        System.out.println("Registration");
+        System.out.println("Please Enter your Name");
+        String userName = scanner.next();
+        myUser = lib.newMember(userName);
+        System.out.println("Registration Registration.");
+        System.out.println("Welcome, " + myUser.getUserName());
+        System.out.println("Your new ID: "+ myUser.getUserID());
     }
 }
