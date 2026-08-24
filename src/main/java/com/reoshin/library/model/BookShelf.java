6962bbd20f287e63863cd7fbad3897efbde69cb1
reoshin <s_reo6@outlook.com>
2026-08-24 22:38:30 +0900

    Improved behaviour for Bookshelf class

--- src/main/java/com/reoshin/library/model/BookShelf.java
+++ src/main/java/com/reoshin/library/model/BookShelf.java
@@ -3,28 +3,38 @@ package com.reoshin.library.model;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.reoshin.library.exceptions.NoSuchItemException;
+
 public class BookShelf implements LoanObserver {
-    private HashMap<String, LibraryItem> items;
+    private HashMap<Category, LibraryItem> items = new HashMap<>();
     private ArrayList<LibraryItem> loanedItems;
     
-    public void addNewItem(LibraryItem item) {
-        // stub
+
+    // EFFECTS: add item into BookShelf, sorted by category
+    public void addItem(LibraryItem item) {
+        items.put(item.getCategory(), item);
     }
 
     public LibraryItem findItemByTitle(String title) {
         return null; // stub
     }
 
-    public ArrayList<LibraryItem> findItemByCategory(Category Category) {
+    public ArrayList<LibraryItem> searchByCategory(Category Category) {
         return null; // stub
     }
 
-    public ArrayList<LibraryItem> findItemByAuthor(String author) {
+    public ArrayList<LibraryItem> searchByAuthor(String author) {
         return null; // stub
     }
 
-    public LibraryItem findItemByID(String ID) {
-        return null;
+    public LibraryItem searchByID(String ID) throws NoSuchItemException {
+        ArrayList<LibraryItem> allItems = new ArrayList<LibraryItem>(items.values());
+        for (LibraryItem item : allItems) {
+            if (item.getID().equals(ID)) {
+                return item;
+            }
+        }
+        throw new NoSuchItemException();
     }
 
     @Override
